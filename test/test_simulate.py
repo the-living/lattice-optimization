1b6188a31fe0e2946883b0705388cf4d419882f2
dl2653 <damon.lau.ca@gmail.com>
2017-03-08 15:49:25 -0500

    update test files

---
+++ test/test_simulate.py
@@ -0,0 +1,38 @@
+from latoptim.graph import lines2graph
+from latoptim.optimize import optimize
+from latoptim.simulate import *
+
+import os, json
+
+path = "\\".join(os.path.dirname(os.path.realpath(__file__)).split("\\")[:]) + "\\"
+
+with open(path + "model.txt") as f:
+    linesInput = json.loads(f.read())
+
+# parameters for graph computation
+target = 0.5
+minRadius = 0.5
+maxRadius = 3.0
+defRadius = 1.0
+speed = 0.1
+epsilon = .01
+
+
+print("generating graph...")
+
+
+graph = lines2graph(linesInput, defRadius, epsilon)
+
+print("graph nodes:", len( graph.getNodes() ))
+print("graph edges:", graph.getEdges())
+
+
+
+nas_model = getNastranModel(graph)
+
+
+print(nas_model)
+
+# graph = optimize(graph, target, minRadius, maxRadius, speed)
+
+# validate graph
