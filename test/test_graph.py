868f8ff458d7b6b3868c2d40a68e6c4e34b748e7
danilnagy <kidvsking@yahoo.com>
2017-03-09 09:24:06 -0500

    update tests

--- test/test_graph.py
+++ test/test_graph.py
@@ -5,6 +5,7 @@ from latoptim.optimize import optimize
 
 
 path = "\\".join(os.path.dirname(os.path.realpath(__file__)).split("\\")[:]) + "\\"
+fp_new_nas = os.path.join(os.curdir, 'nastran', 'truss_TE.nas')
 
 with open(path + "model.txt") as f:
     lines_input = json.loads(f.read())
@@ -27,6 +28,6 @@ graph = lines2graph(lines_input, def_radius, epsilon)
 print("graph nodes:", len( graph.get_nodes() ))
 print("graph edges:", len( graph.get_edges() ))
 
-graph = optimize(graph, target, min_radius, max_radius, speed, maxSteps)
+graph = optimize(graph, target, min_radius, max_radius, speed, maxSteps, fp_new_nas)
 
 print(graph.get_edge_data())
--- test/test_optimize.py
+++ test/test_optimize.py
@@ -1,10 +1,11 @@
+import os, json
+
 from latoptim.graph import lines2graph
 from latoptim.optimize import optimize
 
-import os, json
-
 
 path = "\\".join(os.path.dirname(os.path.realpath(__file__)).split("\\")[:]) + "\\"
+fp_new_nas = os.path.join(os.curdir, 'nastran', 'truss_TE.nas')
 
 with open(path + "model.txt") as f:
     lines_input = json.loads(f.read())
@@ -16,6 +17,7 @@ max_radius = 3.0
 def_radius = 1.0
 speed = 0.1
 epsilon = .01
+maxSteps = 1
 
 
 print("generating graph...")
@@ -24,8 +26,8 @@ print("generating graph...")
 graph = lines2graph(lines_input, def_radius, epsilon)
 
 print("graph nodes:", len( graph.get_nodes() ))
-print("graph edges:", graph.get_edges())
+print("graph edges:", len( graph.get_edges() ))
 
-graph = optimize(graph, target, min_radius, max_radius, speed)
+graph = optimize(graph, target, min_radius, max_radius, speed, maxSteps, fp_new_nas)
 
-# validate graph
+print(graph.get_edge_data())
